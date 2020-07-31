31d3f71d4acd6795e19d2736c966f8a572a8e694
Pedro Nandi <pedronandi@gmail.com>
2020-07-31 19:45:06 -0300

    Fixing Heroku, attempt #3

--- src/pages/Register/Category/index.js
+++ src/pages/Register/Category/index.js
@@ -16,7 +16,6 @@ function RegisterCategory() {
   categories = Array of categories registered
   setCategory = function to add new category at the array
   this semantics means that...
-  test
   */
   const [categories, setCategory] = useState([]);
 
